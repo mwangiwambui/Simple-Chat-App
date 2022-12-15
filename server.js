89513efbcb96c20561ec47122a326215fed6b2e8
mwangiwambui <wambui54mwangi@gmail.com>
2022-12-16 00:40:31 +0300

    Added the routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const chatModel = require('./model/chat_model');
 
 var app = express();
 var bodyParser = require('body-parser');
+const { sendStatus } = require('express/lib/response');
 
 var server = app.listen(3002, () => {
   console.log('server is running on port', server.address().port);
@@ -13,3 +14,18 @@ var server = app.listen(3002, () => {
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/messages', (req, res) => {
+  Message.find({}, (err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.post('/messages', (req,res) => {
+  var message = new Message(req.body);
+  message.save((err)=>{
+    if(err)
+      sendStatus(500);
+      res.sendStatus(200);
+  })
+})
